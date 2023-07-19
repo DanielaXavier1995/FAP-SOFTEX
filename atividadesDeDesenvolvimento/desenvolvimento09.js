097d4f05f80339b112bf786251e2f12f5250188c
Daniela Xavier <116307469+DanielaXavier1995@users.noreply.github.com>
2023-07-19 17:06:22 +0000

    Desenvolvimento 09

---
+++ atividadesDeDesenvolvimento/desenvolvimento09.js
@@ -0,0 +1,15 @@
+var nome = "Daniela"
+var idade = 28
+var altura = "1.64"
+var maior_de_idade = null
+
+if (idade >= 18) {
+    maior_de_idade = true;
+} else {
+    maior_de_idade = false;
+}
+
+console.log("Seu nome é: ", nome);
+console.log("Sua idade é: ", idade);
+console.log("Sua altura é: ", altura);
+console.log("Você é maior de idade? ", maior_de_idade);
