64990d57f8f03f15d58f6864efceb7c31e55c294
Daniela Xavier <116307469+DanielaXavier1995@users.noreply.github.com>
2023-08-03 19:36:15 +0000

    resolução do-while

---
+++ atividadesDeClasse/doWhile01.js
@@ -0,0 +1,9 @@
+//imprima os números de 1 a 10
+let i = 1;
+
+do {
+   console.log(i);
+   i++;
+} while (i <= 10) 
+
+
---
+++ atividadesDeClasse/dowhile02.js
@@ -0,0 +1,8 @@
+//calcule a soma dos números de 1 a 100
+let n = 1, soma = 0;
+
+while (n <= 100) {
+     soma += n;
+     n++;
+}
+console.log(soma);
