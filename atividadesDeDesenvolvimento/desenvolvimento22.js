a54e1b272f6bef04f45a01e9283a3441f2649b7f
Daniela Xavier <danielamxsoares@gmail.com>
2023-08-07 14:47:53 -0300

    atividade de matrizes

--- atividadesDeDesenvolvimento/desenvolvimento22.js
+++ atividadesDeDesenvolvimento/desenvolvimento22.js
@@ -1,2 +1,24 @@
+/*
+
+Nesta atividade, você deve criar um código em JavaScript com uma matriz 
+bidimensional para armazenar informações sobre animais e as suas características.
+
+Cada linha deve representar um animal e cada coluna terá as diferentes informações
+ sobre ele, como nome, espécie e idade.
+
+*/ 
 
 //Criando a matriz:
+
+const animais = [
+    ["gato", "felino", "chico", 3],
+    ["cachorro", "mamifero", "marvin", 5],
+    ["coelho", "mamifero", "ted", 1]
+];
+
+for(let i = 0; i < animais.length; i++){
+    for(let j = 0; j<animais[i].length; j++){
+        console.log(animais[i][j]);
+    }
+    console.log("-------------------")
+}
