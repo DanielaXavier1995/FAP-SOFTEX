3aea638f6386464d234e5537a1ef7c5f31569a4f
Daniela Xavier <116307469+DanielaXavier1995@users.noreply.github.com>
2023-08-08 18:24:46 +0000

    chamando os inputs fora da função

--- atividadesDeClasse/funcao03.js
+++ atividadesDeClasse/funcao03.js
@@ -1,14 +1,15 @@
 
 const input = require("readline-sync")
 
+    const nome = input.question('Digite o seu nome: ');
+    const idade = input.questionInt('Digite a sua idade: ');
+    const dia = input.questionInt('Digite o dia: ');
+    const mes = input.question('Digite o mes: ');
+    const ano = input.questionInt('Digite o ano: ');
+
 function exibirSaudacao(nome, idade, dia, mes, ano) {
-    nome = input.question('Digite o seu nome: ');
-    idade = input.questionInt('Digite a sua idade: ');
-    dia = input.questionInt('Digite o dia: ');
-    mes = input.question('Digite o mes: ')
-    ano = input.questionInt('Digite o ano: ')
     console.log('--------- SAIDA ---------')
     console.log(`Olá, ${nome}, boa tarde, percebi que hoje ${dia} de ${mes} de ${ano}, voce tem ${idade} anos!`);
 }
 
-exibirSaudacao();
+exibirSaudacao(nome, idade, dia, mes, ano);
