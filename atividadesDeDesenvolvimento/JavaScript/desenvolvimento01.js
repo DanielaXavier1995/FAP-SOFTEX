47928be3a372169e3389823498230c5d00bccd94
Daniela Xavier <danielamxsoares@gmail.com>
2023-08-15 18:53:26 -0300

    desenvolvimento01 para o módulo de JavaScript

---
+++ atividadesDeDesenvolvimento/JavaScript/desenvolvimento01.js
@@ -0,0 +1,4 @@
+
+console.log("Ambiente de desenvolvimento pronto para trabalhar com ",
+            "o javaScript, a IDE escolhida foi o Visual Studio Code, ",
+            "devido a sua integração e facilidade para codar em JavaScript");
