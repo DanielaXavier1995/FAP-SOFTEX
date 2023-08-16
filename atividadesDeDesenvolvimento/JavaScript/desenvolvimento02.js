90b5fe707bf70c486b361a1cd8e0f2c980ad4bd1
Daniela Xavier <danielamxsoares@gmail.com>
2023-08-16 19:17:55 -0300

    resolução atividade de desenvolvimento

---
+++ atividadesDeDesenvolvimento/JavaScript/desenvolvimento02.js
@@ -0,0 +1,25 @@
+
+//Variáveis com saída undefined
+let VariavelBoolean
+let VariavelString
+let VariavelBigInt
+let VariavelSymbol
+
+console.log(VariavelBoolean, VariavelString, VariavelBigInt, VariavelSymbol)
+
+//Variáveis com saída atribuindo valor nulo
+VariavelBoolean = null
+VariavelString = null
+VariavelBigInt = null
+VariavelSymbol = null
+
+console.log(VariavelBoolean, VariavelString, VariavelBigInt, VariavelSymbol)
+
+//Variáveis com saída atribuindo valores válidos
+VariavelBoolean = true
+VariavelString = "Hello World!"
+VariavelBigInt = 150
+VariavelSymbol = Symbol("dani")
+
+console.log(VariavelBoolean, VariavelString, VariavelBigInt, VariavelSymbol)
+
