8a2852bd36d66d012167fd0fa243e1595bba47b8
Daniela Xavier <danielamxsoares@gmail.com>
2023-08-17 10:25:02 -0300

    tentativa de resolução do desenvolvimento 03

---
+++ atividadesDeDesenvolvimento/JavaScript/desenvolvimento03.js
@@ -0,0 +1,17 @@
+
+const input = require('readline-sync')
+
+//let nome = input.question("Digite seu nome: ")
+//let salario = input.questionFloat("Digite o seu salario: ")
+//let idade = input.questionInt("Digite a sua idade: ")
+//let diploma = input.question("Voce tem diploma? (true / false) ")
+let teste = true
+
+//while(!diploma.typeof) {
+   // console.log("Tipo de dado inválido")
+    //diploma = input.question("Voce tem diploma? (true / false) ")
+//}
+
+console.log(teste.typeof === "number")
+
+//console.log(nome, salario, idade, diploma)
