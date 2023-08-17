3e5ec80bb4ab4b5d3bffe37775074b4d2ace2d95
Daniela Xavier <116307469+DanielaXavier1995@users.noreply.github.com>
2023-08-17 19:58:25 +0000

    resolução da atividade de desenvolvimento

--- atividadesDeDesenvolvimento/JavaScript/desenvolvimento03.js
+++ atividadesDeDesenvolvimento/JavaScript/desenvolvimento03.js
@@ -1,12 +1,31 @@
 
 const input = require('readline-sync')
 
+console.log("============================")
+
 let nome = input.question("Digite seu nome: ")
 let salario = input.questionFloat("Digite o seu salario: ")
 let idade = input.questionInt("Digite a sua idade: ")
 let diploma = input.question("Voce tem diploma? (true / false) ")
-let tipo = diploma
+let resp;
 
-if(typeof tipo == "boolean"){console.log(nome, salario, idade, diploma)} 
-else {console.log(`Tipo inválido!`)}
+do {
+if (diploma == 'true' || diploma == 'false') {
+    resp = false;
+} else {
+    console.log(`Tipo inválido, o diploma não é boolean! Tente novamente!`)
+    diploma = input.question("Voce tem diploma? (true / false) ")
+    resp = true;
+}
+} while(resp == 'true');
+
+    console.log("=====================");
+    console.log(`Nome: ${nome}`)
+    console.log(`Salário: ${salario}`)
+    console.log(`Idade: ${idade}`)
+    console.log(`Diploma: ${diploma}`)
+
+
+
+
 
