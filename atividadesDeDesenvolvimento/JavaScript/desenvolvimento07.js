c8ffde4351b1a5c53c9a66dde07597a395c54b0b
Daniela Xavier <danielamxsoares@gmail.com>
2023-08-24 19:06:05 -0300

    desenvolvimento 07

---
+++ atividadesDeDesenvolvimento/JavaScript/desenvolvimento07.js
@@ -0,0 +1,21 @@
+const input = require('readline-sync')
+
+function calculoMedia() {
+    let nota1 = input.questionFloat('Digite a primeira nota: ')
+    let nota2 = input.questionFloat('Digite a segunda nota: ')
+
+    let media = (nota1 + nota2)/2
+    console.log(`A média é: ${media}`)
+}
+
+function calculoMedia(nota1, nota2) {
+    let media = (nota1 + nota2)/2
+    return console.log(`A média é: ${media}`)
+}
+
+//const calculoMediaArrow = (nota1 = 7, nota2 = 7) => (nota1 + nota2)/2
+const calcularMedia = (nota1, nota2) => ((nota1 + nota2)/2)
+
+//calculoMedia ()
+//calculoMedia(7, 7)
+console.log(`A média é: ${calcularMedia(7,7)}`);
