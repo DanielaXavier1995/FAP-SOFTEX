3f29aa7f69ea4be725953f08263e92b315c0a562
Daniela Xavier <danielamxsoares@gmail.com>
2023-09-11 11:44:19 -0300

    vetor e matriz

---
+++ atividadesDeDesenvolvimento/JavaScript/objeto/desenvolvimento09.js
@@ -0,0 +1,6 @@
+let tabelaFuncionario = [
+  ["Jose", 19, 1900.0, true][("David", 22, 2500.0, false)][
+    ("Maria", 25, 3400.0, true)
+  ][("Igor", 18, 2100.0, false)],
+];
+console.log(`Funcionário 1: ${tabelaFuncionario[0][0]}`);
